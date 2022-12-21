20654ecc4141b3a4ad71560c9945125ee71c58e9
GargiSinghRao12 <gargisinghrao12@gmail.com>
2022-12-21 09:40:00 +0200

    minor changes

---
+++ build/src/CMakeFiles/qr-to-png.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/cpp-qr-to-png_price_arora/src/QrToPng.cpp" "/workspaces/cpp-qr-to-png_price_arora/build/src/CMakeFiles/qr-to-png.dir/QrToPng.cpp.o"
+  "/workspaces/cpp-qr-to-png_price_arora/src/main.cpp" "/workspaces/cpp-qr-to-png_price_arora/build/src/CMakeFiles/qr-to-png.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../libs/tinypngout"
+  "../libs/QR-Code-generator"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/cpp-qr-to-png_price_arora/build/libs/tinypngout/CMakeFiles/tinypngoutput.dir/DependInfo.cmake"
+  "/workspaces/cpp-qr-to-png_price_arora/build/libs/QR-Code-generator/CMakeFiles/qr-code-generator.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
